ca54581451d60a318e91c082831c3db355e5d6c5
Mehdi <davoudpour2000@gmail.com>
2023-04-29 19:06:07 +0330

    complete front end

--- pythonProject/Functions.py
+++ pythonProject/Functions.py
@@ -8,7 +8,7 @@ def read():
     return to_do_list
 
 
-def watch():
+'''def watch():
     with open('To Do List.txt', 'r') as file:
         to_do_list = file.readlines()
     print('\n\n')
@@ -16,38 +16,25 @@ def watch():
         i = to_do_list.index(item) + 1
         line = f"{i}. {item}".replace('\n', '')
         print(line)
-    print('\n\n')
+    print('\n\n') '''
 
 
-def add(to_do_list):
-    to_do_new = input("type new work to do and press enter: "
-                      "\nfor back to main menu enter 0: ".title())
-    if to_do_new == '0':
-        print(message_back)
-    else:
-        to_do_new = '\n' + to_do_new.title()
-        to_do_list.append(to_do_new)
-        with open('To Do List.txt', 'w') as file:
-            file.writelines(to_do_list)
+def add(to_do_new):
+
+    to_do_list = read()
+    to_do_new = '\n' + to_do_new.title()
+    to_do_list.append(to_do_new)
+    with open('To Do List.txt', 'w') as file:
+        file.writelines(to_do_list)
 
 
-def delete(to_do_list):
-    try:
-        number_deleted = int(input("type the number that you want to"
-                                   " delete from above and"
-                                   " press enter"
-                                   "\nfor back to main menu enter 0: ".title()))
-        if number_deleted == 0:
-            print(message_back)
-        else:
-            number_deleted = number_deleted - 1
-            del to_do_list[number_deleted]
+def delete(to_do_to_delete):
+    to_do_list = read()
+    to_do_list.remove(to_do_to_delete)
 
-            with open('To Do List.txt', 'w') as file:
-                file.writelines(to_do_list)
-    except ValueError:
-        print(message_wrong)
-        delete(to_do_list)
+    with open('To Do List.txt', 'w') as file:
+        file.writelines(to_do_list)
+
 
 
 def edit(to_do_list):
--- pythonProject/To Do - Front End.py
+++ pythonProject/To Do - Front End.py
@@ -1,19 +1,38 @@
-import PySimpleGUI
+import PySimpleGUI as Sg
+from Functions import read, add, delete
 
-'''label_height = PySimpleGUI.Text("Your Height: ")
-label_weight = PySimpleGUI.Text("Your Weight: ")
-inputBox_height = PySimpleGUI.InputText(tooltip="Height")
-inputBox_weight = PySimpleGUI.InputText(tooltip="Weight")'''
 
-button_Show = PySimpleGUI.Button("Show")
-button_Add = PySimpleGUI.Button("Add")
-button_Delete = PySimpleGUI.Button("Delete")
-button_Edit = PySimpleGUI.Button("Edit")
+def show():
+    to_do_list = read()
+    window['-LIST-'].update(values=to_do_list)
 
-window = PySimpleGUI.Window("BMI Calculator",
-                            layout=[[button_Show],
-                                    [button_Add],
-                                    [button_Delete],
-                                    [button_Edit]])
 
-window.read()
+button_Show = Sg.Button("Show")
+button_Add = Sg.Button("Add")
+textBox_Add = Sg.InputText(tooltip='enter new to do'.title(), key='-Add-')
+button_Delete = Sg.Button("Delete")
+list_box = Sg.Listbox(values=[], size=(20, 10), key='-LIST-')
+
+window = Sg.Window("BMI Calculator",
+                   layout=[[button_Show],
+                           [button_Add, textBox_Add],
+                           [button_Delete],
+                           [list_box]])
+while True:
+    event, value = window.read()
+    if event == 'Show':
+        show()
+    elif event == 'Add':
+        to_do_new = value['-Add-']
+        print(to_do_new)
+        add(to_do_new)
+        show()
+    elif event == 'Delete':
+        to_do_to_delete = value['-LIST-'][0]
+        print(to_do_to_delete)
+        delete(to_do_to_delete)
+        show()
+    else:
+        exit()
+
+
