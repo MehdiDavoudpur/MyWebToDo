29268731169bfbc85d125a5eb96635c1986f77a7
Mehdi <davoudpour2000@gmail.com>
2023-05-02 16:08:38 +0330

    initially commit

--- pythonProject/To Do - Front End.py
+++ pythonProject/To Do - Front End.py
@@ -15,11 +15,11 @@ textBox_Add = Sg.InputText(tooltip='enter new to do'.title(), key='-Add-')
 button_Delete = Sg.Button("Delete")
 list_box = Sg.Listbox(values=to_do_list, size=(20, 10), key='-LIST-')
 
-window = Sg.Window("BMI Calculator",
+window = Sg.Window("to do app".title(),
                    layout=[[button_Show],
                            [button_Add, textBox_Add],
                            [button_Delete],
-                           [list_box]])
+                           [list_box]], font=('Times New Roman', 20))
 
 while True:
     event, value = window.read()
@@ -29,8 +29,12 @@ while True:
     elif event == 'Add':
         to_do_new = value['-Add-']
         print(to_do_new)
-        add(to_do_new)
-        show()
+        if to_do_new != '':
+            add(to_do_new)
+            show()
+            window['-Add-'].update(value='')
+        else:
+            Sg.Popup('please type a to do to add it'.title())
     elif event == 'Delete':
         try:
             to_do_to_delete = value['-LIST-'][0]
