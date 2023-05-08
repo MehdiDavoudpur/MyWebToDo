d0668aaa3f7c93c522d1741159c89e8c217ea0a1
Mehdi <davoudpour2000@gmail.com>
2023-05-08 19:27:03 +0330

    create add in web todo

--- pythonProject/Functions.py
+++ pythonProject/Functions.py
@@ -10,9 +10,9 @@ def read(a_filepath):
 
 def add(a_filepath, to_do_new):
     to_do_list = read(a_filepath)
-    to_do_new = '\n' + to_do_new.title()
+    to_do_new = to_do_new.title() + '\n'
     to_do_list.append(to_do_new)
-    with open('To_Do_List.txt', 'w') as file:
+    with open(a_filepath, 'w') as file:
         file.writelines(to_do_list)
 
 
@@ -24,7 +24,7 @@ def delete(a_filepath, to_do_to_delete):
         file.writelines(to_do_list)
 
 
-def edit(to_do_list):
+def edit(a_filepath, to_do_list):
     try:
         edited = input("type the number that you want to"
                        " edit from above and"
@@ -38,7 +38,7 @@ def edit(to_do_list):
             to_do_edited = edited[3:].title()
             to_do_list[number_edited - 1] = f"{to_do_edited} \n"
 
-            with open('To_Do_List.txt', 'w') as file:
+            with open(a_filepath, 'w') as file:
                 file.writelines(to_do_list)
     except ValueError:
         print(message_wrong)
--- pythonProject/webTodo.py
+++ pythonProject/webTodo.py
@@ -1,11 +1,19 @@
 import streamlit as st
-from Functions import read, delete
+from Functions import read, delete, add
 
 my_filepath = "C:/Users/Mehdi/PycharmProjects/pythonProject/To_Do_List.txt"
+
+new_todo = st.text_input(label="enter new todo".title())
+
+button_add = st.button('Add')
 
 todos = read(my_filepath)
 
 for key, todo in enumerate(todos):
-    checkbox = st.checkbox(todo)
+    checkbox = st.checkbox(todo, key=key)
     if checkbox:
         delete(my_filepath, todos[key])
+        st.experimental_rerun()
+if button_add:
+    add(my_filepath, new_todo)
+    st.experimental_rerun()
