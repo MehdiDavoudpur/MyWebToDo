ef5833381da471b437fc08149dd84bab0d296ed4
Mehdi <davoudpour2000@gmail.com>
2023-05-12 11:14:31 +0330

    fix an error

--- WebToDo/Functions.py
+++ WebToDo/Functions.py
@@ -22,24 +22,3 @@ def delete(a_filepath, to_do_to_delete):
 
     with open(a_filepath, 'w') as file:
         file.writelines(to_do_list)
-
-
-def edit(a_filepath, to_do_list):
-    try:
-        edited = input("type the number that you want to"
-                       " edit from above and"
-                       " new work to do instead of it"
-                       " and press enter: "
-                       "\nfor back to main menu enter 0: ".title())
-        if edited == '0':
-            print(message_back)
-        else:
-            number_edited = int(edited[0:1])
-            to_do_edited = edited[3:].title()
-            to_do_list[number_edited - 1] = f"{to_do_edited} \n"
-
-            with open(a_filepath, 'w') as file:
-                file.writelines(to_do_list)
-    except ValueError:
-        print(message_wrong)
-        edit(to_do_list)
--- WebToDo/webTodo.py
+++ WebToDo/webTodo.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from Functions import read, delete, add
 
-my_filepath = "C:/Users/Mehdi/PycharmProjects/pythonProject/To_Do_WebApp/To_Do_List.txt"
+my_filepath = "C:/Users/Mehdi/PycharmProjects/WebToDo/To_Do_List.txt"
 
 
 def get_new_todo():
